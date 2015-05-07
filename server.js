18fc1446f66e4dd3b03cc07f4b00e2a2d0a378ff
Ivan <ivan.lapshin@39parrot.net>
2015-05-07 22:25:48 +0400

    new server

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');           // call express
+var flights = require('./db/Flights_Full.json');
+var port    = process.env.PORT || 8080;     // set our port
+var _       = require("underscore");
+var cors    = require('cors');
+    app.use(cors());
+
+
+var router = express.Router();              // get an instance of the express Router
+
+router.use(function(req, res, next) {       // middleware to use for all requests
+    console.log('Something is happening.');
+    next();                                 // make sure we go to the next routes and don't stop here
+});
+
+router.get('/:carrier_code', function(req, res) {     //(accessed at GET http://localhost:8080/flight_subscription)
+    var flights2 = _.filter(flights, function(flight){ return flight.carrier_code === req.params.carrier_code; });
+    if(flights2.length > 0) {
+        res.json(flights2);
+    }else{
+        res.send('page not found', 404);
+    }
+});
+
+router.get('', function(req, res) {
+    res.json(flights);
+});
+
+app.use('/flights_subscriptions', router);    // all of our routes will be prefixed with /api
+
+app.get('*', function(req, res){
+    res.send('Page not Found', 404);
+});
+
+
+app.listen(port);                       // START THE SERVER
+    console.log('Magic happens on port ' + port);
