12d3e27c87e82282f40a78ed1c920969ce6cfa90
piton182 <dmitry.kouznetsvo@gmail.com>
2015-05-07 20:59:36 +0200

    refactor

--- server.js
+++ server.js
@@ -4,35 +4,25 @@ var flights = require('./db/Flights_Full.json');
 var port    = process.env.PORT || 8080;     // set our port
 var _       = require("underscore");
 var cors    = require('cors');
-    app.use(cors());
 
+app.use(cors());
 
-var router = express.Router();              // get an instance of the express Router
-
-router.use(function(req, res, next) {       // middleware to use for all requests
-    console.log('Something is happening.');
-    next();                                 // make sure we go to the next routes and don't stop here
+app.get('/carriers', function(req, res) {
+    res.send('Not supported yet. Coming soon...\n');
 });
 
-router.get('/:carrier_code', function(req, res) {     //(accessed at GET http://localhost:8080/flight_subscription)
+app.get('/flight_subscriptions/:carrier_code', function(req, res) {
     var flights2 = _.filter(flights, function(flight){ return flight.carrier_code === req.params.carrier_code; });
     if(flights2.length > 0) {
         res.json(flights2);
     }else{
-        res.send('page not found', 404);
+        res.send('Page not Found\n', 404);
     }
 });
-
-router.get('', function(req, res) {
-    res.json(flights);
-});
-
-app.use('/flight_subscriptions', router);    // all of our routes will be prefixed with /api
 
 app.get('*', function(req, res){
     res.send('Page not Found\n', 404);
 });
 
-
 app.listen(port);                       // START THE SERVER
-    console.log('Magic happens on port ' + port);
+console.log('Magic happens on port ' + port);
