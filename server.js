88c62d3b714426f5847b7acf555c17c3411951c2
Ivan <ivan.lapshin@39parrot.net>
2015-05-11 02:09:25 +0400

    added basic auth

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-var express = require('express');           // call express
-var app     = express();                    // define our app using express
-var flights = require('./db/Flights_Full.json');
-var port    = process.env.PORT || 8080;     // set our port
-var _       = require("underscore");
-var cors    = require('cors');
-
+var express     = require('express');           // call express
+var app         = express();                    // define our app using express
+var flights     = require('./db/Flights_Full.json');
+var port        = process.env.PORT || 8080;     // set our port
+var _           = require("underscore");
+var cors        = require('cors');
+var basicAuth   = require('basic-auth');
 app.use(cors());
 
+var auth = function (req, res, next) {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.send(401);
+    }
+    var user = basicAuth(req);
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(res);
+    }
+    if (user.name === 'foo' && user.pass === 'bar') {
+        return next();
+    } else {
+        return unauthorized(res);
+    }
+};
 app.get('/carriers', function(req, res) {
     var carriersSort = [];
     for (var i=0; i<flights.length; i++){
@@ -16,9 +31,13 @@ app.get('/carriers', function(req, res) {
     res.json(carriersSort);
 });
 
-app.get('/flight_subscriptions/:carrier_code', function(req, res) {
+app.get('/flight_subscriptions/:carrier_code', auth, function(req, res) {
     var flights2 = _.where(flights, {carrier_code: req.params.carrier_code});
-        res.json(flights2)
+        if(flights2.length > 0){
+            res.json(flights2)
+        }else{
+            res.send('Page not Found\n', 404);
+        }
 });
 
 app.get('*', function(req, res){
